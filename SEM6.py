2cec0be952f8bb816fb06e5e1b0e2137db8c1996
Slarkvo <slarkvo@gmail.com>
2024-10-14 22:45:46 -0500

    CORRIGIENDO2

--- SEM6.py
+++ SEM6.py
@@ -1,21 +1,20 @@
 import streamlit as st
+#titulo de la aplicacion
+st.title("Efercicios con bucles basicos en python")
 
-#tìtulo de la aplicaciòn
-st.title("ejercicios con bucles bàsicos en python")
-
-#ejercicio 1: imprimir 10 veces 'Hola mundo'
-st.subheader("Ejercicio1: imprimir 'Hola Mundo' 10 Veces")
-if st.button("Ejecutar Ejercicio 1")
+#ejercicio 1: imprimir 10 veces 'Hola Mundo'
+st.title("Ejercicio 1: imprimir 'Hola Mundo'10 veces")
+if st.button("Ejecutar Ejercicio 1"):
     for i in range(10):
-        st.write("Hola mundo")
-#Ejercicio 2: Imprimir los primeros 10 nùmeros
-st.subheader("Ejercicio 2: Imprimir los primeros 10 nùmeros")
-is st.button("Ejecutar Ejercicio 2")
+        st.write("Hola Mundo")
+#ejercicio 2: Imprimir los primeros 10 numeros
+st.subheader("Ejercicio 3: Imprimir los primeros 10 numeros")
+if st.button("Ejecutar Ejercicio 2"):
     for i in range(1, 11):
         st.write(i)
 #Ejercicio 3: Tabla de multiplicar
-St. subheader("Ejercicio 3: Imprimir la tabla de multiplicar del nùmero ingresado")
-num = st.number_imput("Ingrese un nùmero para ver su tabla de multiplicar del 1 al 12" min_value=1)
-if st.button("Ejecutar Ejercicio3"):
-    for i in range(1, 13):
-        st.write(f"{num} x {i} = {num * i}")
+st.subheader("Ejercicio 3: Imprimir la tabla de multiplicar del numero ingresado")
+num = st.number_input("Ingrese un numero para ver su tabla de multiplicar del 1 al 12", min_value=1)
+if st.button("ejercutar Ejercicio 3"):
+    for i in range(1,13):
+        st.write(f"{num} x {i} = {num*i}")
